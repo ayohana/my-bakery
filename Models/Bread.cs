73a97bbd6f066178314f423a853bb38b32d3d531
Adela <adela.yohana@gmail.com>
2020-02-28 10:46:42 -0800

    Add regex to find whitespaces in user input

--- Models/Bread.cs
+++ Models/Bread.cs
@@ -5,6 +5,15 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    
+    public int InputOrder { get; set; }
+    public int CostPerLoaf { get; set; }
+    public int TotalBreadCost { get; set; }
+
+    public Bread(int inputOrder)
+    {
+      InputOrder = inputOrder;
+      CostPerLoaf = 5;
+      TotalBreadCost = InputOrder * CostPerLoaf;
+    }
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using Bakery.Models;
 
 namespace Bakery
@@ -7,12 +8,14 @@ namespace Bakery
   public class Program
   {
     public static bool stillShopping = true;
+    public static bool stillBrowsing = false;
     public static void Main()
     {
       WelcomeMessage();
+
       while(stillShopping)
       {
-        MainMenu();
+        ConsoleMenu();
       }
     }
 
@@ -27,26 +30,82 @@ namespace Bakery
       Console.WriteLine("the finest local sugar, butter and flour ingredients. Enjoy!");
     }
 
-    public static void MainMenu()
+    public static void ConsoleMenu()
     {
       Console.WriteLine(">>> Please type in a NUMBER from our menu options below:");
       Console.WriteLine("1. Have a look at our baked goods!");
       Console.WriteLine("2. Exit");
-      string input = Console.ReadLine();
-      switch(input)
+      string inputConsoleMenu = Console.ReadLine();
+
+      switch(inputConsoleMenu)
       {
         case "1":
-          Console.WriteLine("Della's Baked Goods");
+          stillBrowsing = true;
+          while(stillBrowsing)
+          {
+            BakeryMenu();
+          }
           break;
         case "2":
           Console.WriteLine(">>> Goodbye! See you next time!");
-          Environment.Exit(0);
+          stillShopping = false;
           break;
         default:
           Console.WriteLine(">>> Invalid input. Please try again.");
           break;
       }
+    }
 
+    public static void BakeryMenu()
+    {
+      Console.WriteLine("===== Della's Baked Goods =====");
+      Console.WriteLine("World's Best Bread $5/loaf");
+      Console.WriteLine("World's Best Pastry $2/piece");
+      Console.WriteLine("When you're ready for checkout, enter \"checkout\" to see your");
+      Console.WriteLine("final cost or enter \"exit\" to cancel/go back to Main Menu.");
+      Console.WriteLine("Enter your order: [QUANTITY] [ITEM] (example: 2 bread)");
+
+      Regex rx = new Regex("\\s+");
+      string inputOrder = (Console.ReadLine()).ToLower();
+      if (rx.IsMatch(inputOrder))
+      {
+        Console.WriteLine("input has whitespaces");
+      }
+      
+      // if (inputOrder.Length < 1)
+      // {
+      //   switch(inputOrder[0])
+      //   {
+      //     case "checkout":
+      //       Console.WriteLine("Here's your total = ...");
+      //       stillBrowsing = false;
+      //       break;
+      //     case "exit":
+      //       Console.WriteLine("You are done browsing our menu.");
+      //       stillBrowsing = false;
+      //       break;
+      //     default:
+      //       Console.WriteLine(">>> Invalid input. Please try again.");
+      //       break;
+      //   }
+      // }
+      // else
+      // {
+      //   inputOrder = inputOrder.Split(" ");
+      //   int inputQuantity = int.Parse(inputOrder[0]);
+      //   string inputItem = inputOrder[1];
+      //   switch(inputItem)
+      //   {
+      //     case "bread":
+      //       break;
+      //     case "pastry":
+      //       break;
+      //     default:
+      //       Console.WriteLine(">>> Invalid input. Please try again.");
+      //       break;
+      //   }
+      // }
+      
     }
   }
 }
